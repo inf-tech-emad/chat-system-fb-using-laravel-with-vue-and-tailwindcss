357916ca2192569887365e0a158cb3fc1dc907fd
unknown <talmd440@gmail.com>
2023-07-15 18:45:54 +0300

    Create A Chat Model With MCRFSRP

---
+++ app/Http/Controllers/ChatController.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\Auth\Chat\DeleteChatRequest;
+use App\Http\Requests\Auth\Chat\StoreChatRequest;
+use App\Http\Requests\Auth\Chat\UpdateChatRequest;
+use App\Models\Chat;
+
+class ChatController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $data = [
+            'chats'=>Chat::all(),
+        ];
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreChatRequest $request)
+    {
+        if ($request->store()){
+            return back();
+        }
+        return back()->withErrors([''=>'']);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Chat $chat)
+    {
+        $data = [
+            'chat'=>$chat,
+        ];
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Chat $chat)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateChatRequest $request, Chat $chat)
+    {
+        if ($request->update($chat)){
+            return back();
+        }
+        return back()->withErrors([''=>'']);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(DeleteChatRequest $request,Chat $chat)
+    {
+        if ($request->delete($chat)){
+            return back();
+        }
+        return back()->withErrors([''=>'']);
+    }
+}
---
+++ app/Http/Requests/Auth/Chat/StoreChatRequest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Requests\Auth\Chat;
+
+use App\Models\Chat;
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreChatRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules(): array
+    {
+        return [
+            'name'=>'required|string',
+        ];
+    }
+    public function store(): bool
+    {
+        $chat = new Chat();
+        $chat->name = $this->name;
+        return $chat->save();
+    }
+}
